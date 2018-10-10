fa851b0102ce23c410431d839c2dab17f913c56c
Sedoy Andrey <SedoyAndrey1984@gmail.com>
2018-10-10 19:14:38 +0300

    HW-3 JS

--- public/views/javascript/howework-1/js/script.js
+++ public/views/javascript/howework-1/js/script.js
@@ -7,3 +7,9 @@
 
  var namePlanet = 'Земля';
  var userName = 'Петя';
+
+let quantityFrameSecond // количество кадров в секунду;
+let hyphen // соеденительный знак между словами;
+let viewDocument // режим просмотра документа;
+let lastPageNav // необходимость показа последней страницы постраничной навигации;
+
