82bf840d2c172eee0dbd1c558b32b69f1d04086b
Sedoy Andrey <SedoyAndrey1984@gmail.com>
2018-10-23 11:33:44 +0300

    www

---
+++ public/views/javascript/howework-5/js/script.js
@@ -0,0 +1,15 @@
+ 'use strict'
+
+ var admin = name;
+ var name = 'Василий';
+
+ alert(admin);
+
+ var namePlanet = 'Земля';
+ var userName = 'Петя';
+
+let quantityFrameSecond // количество кадров в секунду;
+let hyphen // соеденительный знак между словами;
+let viewDocument // режим просмотра документа;
+let lastPageNav // необходимость показа последней страницы постраничной навигации;
+
